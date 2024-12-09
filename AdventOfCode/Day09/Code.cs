ee256791ddaa255a58abd34ef7ef3b340252172b
Hekkert, Erwin <erwin.hekkert@tauw.com>
2024-12-09 13:43:09 +0100

    Day 9 part 1

---
+++ AdventOfCode/Day09/Code.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Day09
+{
+    public class Code
+    {
+        public long Part1(string line)
+        {
+            var blocks = DiskMapToBlocks(line);
+            var defragmented = Defragment(blocks);
+            return CalculateChecksum(defragmented);
+        }
+
+        public long Part2(string line)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] DiskMapToBlocks(string line)
+        {
+            string[] result = Array.Empty<string>();
+
+            int blockId = 0;
+
+            StringBuilder blocks = new();
+            for (int i = 0; i < line.Length; i++)
+            {
+                int currentSize = result.Length;
+                int newSize = (int)char.GetNumericValue(line[i]) + result.Length;
+                string[] tempArray =  new string[newSize];
+                Array.Copy(result, tempArray, currentSize);
+
+                var blockIdAsString = blockId.ToString();
+                if (i % 2 == 0)
+                {
+                    for (int j = currentSize; j < newSize; j++)
+                    {
+                        tempArray[j] = blockIdAsString;
+                    }
+                    blockId++;
+                }
+                else
+                {
+                    for(int j = currentSize; j < newSize; j++)
+                    {
+                        tempArray[j] = ".";
+                    }
+                }
+
+                result = tempArray;
+            }
+
+            return result;
+        }
+
+        public string[] Defragment(string[] line)
+        {
+            int index1 = 0;
+            int index2 = line.Length - 1;
+
+            do
+            {
+                if (line[index1] == ".")
+                {
+                    string replaceWith = "";
+                    do
+                    {
+                        replaceWith = line[index2];
+                        index2--;
+                    } while (replaceWith == "." && index2 > index1);
+
+                    if (replaceWith != ".")
+                    {
+                        line[index2+1] = line[index1];
+                        line[index1] = replaceWith;
+                    }
+                }
+
+                index1++;
+            } while (index1 < index2);
+
+            return line;
+        }
+
+        public long CalculateChecksum(string[] line)
+        {
+            long checksum = 0;
+            int multiplier = 0;
+
+            do
+            {
+                checksum += Convert.ToInt16(line[multiplier]) * multiplier;
+                multiplier++;
+            } while (line[multiplier] != ".");
+
+            return checksum;
+        }
+    }
+}
---
+++ AdventOfCodeTest/Day09.cs
@@ -0,0 +1,80 @@
+﻿using AdventOfCode.Day09;
+
+namespace AdventOfCodeTest
+{
+    public class Day09
+    {
+        private string dayNumber = "09";
+
+        private Code code = new Code();
+
+        [Theory]
+        [InlineData("12345", "0..111....22222")]
+        [InlineData("2333133121414131402", "00...111...2...333.44.5555.6666.777.888899")]
+        public void TestDiskMapToBlocks(string data, string expectedAsString)
+        {
+            var result = code.DiskMapToBlocks(data);
+
+            var expected = expectedAsString.Select(x => x.ToString()).ToArray();
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("0..111....22222", "022111222......")]
+        [InlineData("00...111...2...333.44.5555.6666.777.888899", "0099811188827773336446555566..............")]
+        public void TestDefragment(string dataAsString, string expectedAsString)
+        {
+            var data = dataAsString.Select(x => x.ToString()).ToArray();
+            var result = code.Defragment(data);
+
+            var expected = expectedAsString.Select(x => x.ToString()).ToArray();
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TestCalculateChecksum()
+        {
+            var dataAsString = "0099811188827773336446555566..............";
+            var data = dataAsString.Select(x => x.ToString()).ToArray();
+            var result = code.CalculateChecksum(data);
+
+            Assert.Equal(1928, result);
+        }
+
+        [Fact]
+        public void Part1Example()
+        {
+            var line = File.ReadAllText(@$".\Day{dayNumber}\ExampleData.aoc");
+            var result = code.Part1(line);
+
+            Assert.Equal(1928, result);
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var line = File.ReadAllText(@$".\Day{dayNumber}\Data.aoc");
+            var result = code.Part1(line);
+
+            Assert.Equal(6288599492129, result);
+        }
+
+        //[Fact]
+        //public void Part2Example()
+        //{
+        //    var lines = File.ReadAllLines(@$".\Day{dayNumber}\ExampleData.aoc");
+        //    var result = code.Part2(lines);
+
+        //    Assert.Equal(34, result);
+        //}
+
+        //[Fact]
+        //public void Part2()
+        //{
+        //    var lines = File.ReadAllLines(@$".\Day{dayNumber}\Data.aoc");
+        //    var result = code.Part2(lines);
+
+        //    Assert.Equal(1293, result);
+        //}
+    }
+}
