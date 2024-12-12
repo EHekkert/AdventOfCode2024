53b4c865e248c0c4817cafd8f9ef144d732bb364
Hekkert, Erwin <erwin.hekkert@tauw.com>
2024-12-12 07:40:15 +0100

    Day 11 part 1 & part 2

---
+++ AdventOfCode/Day11/Code.cs
@@ -0,0 +1,83 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode.Day11
+{
+    public class Code
+    {
+        private Dictionary<(string stone, int numberOfBlinks), BigInteger> _cache = new Dictionary<(string, int), BigInteger>();
+        
+        public long Part1(string line)
+        {
+            var stones = line.Split(' ');
+
+            BigInteger result = 0;
+            foreach (var stone in stones)
+            {
+                result += FindNumberOfStones(stone, 1, 25);
+            }
+
+            return (long)result;
+        }
+
+        public long Part2(string line)
+        {
+            var stones = line.Split(' ');
+
+            BigInteger result = 0;
+            foreach (var stone in stones)
+            {
+                result += FindNumberOfStones(stone, 1, 75);
+            }
+
+            return (long)result;
+        }
+
+        public BigInteger FindNumberOfStones(string stone, int numberOfBlinks, int maxNumberOfBlinks)
+        {
+            if (numberOfBlinks > maxNumberOfBlinks)
+            {
+                return 1;
+            }
+
+            // Memoization: Check if the result is already cached
+            if (_cache.TryGetValue((stone, numberOfBlinks), out BigInteger cachedResult))
+            {
+                return cachedResult;
+            }
+
+            BigInteger result = 0;
+            numberOfBlinks++;
+            if (string.IsNullOrEmpty(stone) || stone == "0")
+            {
+                // Handle the case where stone is zero or empty
+                result = FindNumberOfStones("1", numberOfBlinks, maxNumberOfBlinks);
+            }
+            else
+            {
+                int length = stone.Length;
+
+                if (length % 2 == 0)
+                {
+                    int mid = length / 2;
+                    string left = stone.Substring(0, mid).TrimStart('0');
+                    string right = stone.Substring(mid).TrimStart('0');
+
+                    result += FindNumberOfStones(left, numberOfBlinks, maxNumberOfBlinks);
+                    result += FindNumberOfStones(right, numberOfBlinks, maxNumberOfBlinks);
+                }
+                else
+                {
+                    BigInteger stoneValue = BigInteger.Parse(stone);
+                    BigInteger newStoneValue = stoneValue * 2024;
+                    string newStone = newStoneValue.ToString().TrimStart('0');
+
+                    result = FindNumberOfStones(newStone, numberOfBlinks, maxNumberOfBlinks);
+                }
+            }
+
+            // Store the computed result in the cache
+            _cache[(stone, numberOfBlinks-1)] = result;
+            return result;
+        }
+    }
+}
---
+++ AdventOfCodeTest/Day11.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode.Day11;
+
+namespace AdventOfCodeTest
+{
+    public class Day11
+    {
+        private string dayNumber = "11";
+
+        private Code code = new Code();
+
+        //[Theory]
+        //[InlineData("125 17", "253000 1 7")]
+        //[InlineData("253000 1 7", "253 0 2024 14168")]
+        //[InlineData("253 0 2024 14168", "512072 1 20 24 28676032")]
+        //public void TestStonesAfterBlink(string line, string expected)
+        //{
+        //    var data = line.Split(' ').Select(x => long.Parse(x)).ToArray();
+        //    var expectedData = expected.Split(' ').Select(x => long.Parse(x)).ToArray();
+
+        //    var result = code.StonesAfterBlink(data);
+
+        //    Assert.Equal(expectedData, result);
+        //}
+
+        [Fact]
+        public void Part1Example()
+        {;
+            var line = File.ReadAllText(@$".\Day{dayNumber}\ExampleData.aoc");
+            var result = code.Part1(line);
+
+            Assert.Equal(55312, result);
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var line = File.ReadAllText(@$".\Day{dayNumber}\Data.aoc");
+            var result = code.Part1(line);
+
+            Assert.Equal(218956, result);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var line = File.ReadAllText(@$".\Day{dayNumber}\Data.aoc");
+            var result = code.Part2(line);
+
+            Assert.Equal(259593838049805, result);
+        }
+    }
+}
